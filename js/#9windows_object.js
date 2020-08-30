00b7536d8f0e2ba39efb88560ee8ccac7999758f
ShristiJalan <shristijalan1010@gmail.com>
2020-08-30 22:16:01 +0530

    window objects in js

---
+++ js/#9windows_object.js
@@ -0,0 +1,16 @@
+console.log('This is about window object in JS ');
+let a = window.document;
+// HTML, head, body comes inside document and this is what we call DOM
+
+// alert('hello Shristi');
+// a = prompt('This will destroy your computer. Type your name');
+
+// a = confirm('Are you sure you want to delete this page?');
+a = window.innerHeight; // OR a = innerHeight; both are same
+a = window.innerWidth;
+a = scrollX;  // how much scroll you have done in X
+a = scrollY;  // how much scroll you have done in Y
+a = location;
+a = location.toString();
+a = window.history;
+console.log(a);
